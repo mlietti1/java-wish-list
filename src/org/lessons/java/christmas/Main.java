eff25b2ddb935e3c1301d2591e805b50c408c0a2
mlietti1 <mcf.lietti@outlook.com>
2023-03-16 14:44:34 +0100

    Initial commit

---
+++ src/org/lessons/java/christmas/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        List<String> wishList = new ArrayList<>();
+
+        
+    }
+}
