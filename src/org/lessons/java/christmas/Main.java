33cb715e38d947feed7a23c6dd1263c1e2e28eb6
mlietti1 <mcf.lietti@outlook.com>
2023-03-16 14:50:06 +0100

    init

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,3 +1,5 @@
+package org.lessons.java.christmas;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,6 +11,16 @@ public class Main {
 
         List<String> wishList = new ArrayList<>();
 
-        
+        boolean stop = false;
+
+        while (!stop) {
+            System.out.print("Add wish: ");
+            String item = scan.nextLine();
+            // aggiungo n alla lista
+            wishList.add(item);
+            System.out.println("You have now " + wishList.size() + " wishes.");
+            System.out.print("Stop?(y/n): ");
+            stop = scan.nextLine().equalsIgnoreCase("y");
+        }
     }
 }
